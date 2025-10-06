9fa9be6a97a8df97f3cf271314d59f11b27ab409
John Chrismark B Dacsil <dacsilm@gmail.com>
2025-10-06 18:57:46 +0800

    Blog post Skeleton

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts = Post::all();
+    return view('home', ['posts' => $posts]);
 });
+
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
+
+//blog post route
+Route::post('/create-post', [PostController::class, 'createPost']);
